51c74c8887278fa7589eb29acf68da1208a954c7
Kurt Friars <kfriars@apple.com>
2025-02-24 17:40:28 -0500

    Laravel 10.x specific branch

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,9 +1,5 @@
 <?php
 
-arch()->preset()->php();
-arch()->preset()->laravel();
-arch()->preset()->security();
-
 arch('it will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
     ->each->not->toBeUsed();
